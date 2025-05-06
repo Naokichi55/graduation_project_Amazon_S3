87be5e83da855141b0a48070eae06b2678fd48ee
Naokichi55 <ibosanganba@gmail.com>
2025-05-06 21:40:07 +0900

    画像投稿にバリデーションを記載

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
 	# active strageの画像との関連付け
 	has_many_attached :images
+
+
+  validates :images, content_type: { in: %w[image/jpeg image/gif image/png ],
+	                                    message: "画像のフォーマットはjpeg,gif,png,heicのみです"},
+										size: 				{ less_than: 5.megabytes,
+																	    message: "サイズは5MB以下のサイズです。"}
 end
